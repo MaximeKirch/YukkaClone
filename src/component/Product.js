5363cc70299daac33455cfd3ded2f7aeb619105f
Maxime Kirch <maxime.kirch@gmail.com>
2021-11-01 14:15:37 +0100

    UX Design + comp Product

--- src/component/Product.js
+++ src/component/Product.js
@@ -7,15 +7,15 @@ import Footer from "./Footer";
 // 3327272107259
 // 737628064502
 // 3168930009078
+// 5053990155354
 
-//dataProduct.nutriscore_data
 
 
 function Product(props) {
 
   let barcode = props.match.params.barcode;
   const [dataProduct, setDataProduct]=useState(null);
-  const [ingredient, setIngredient]=useState(true);
+  const [ingredient, setIngredient]=useState('A');
 
   console.log(ingredient);
 
@@ -26,53 +26,6 @@ function Product(props) {
       .then((data)=>setDataProduct(data.product));
     },[barcode]);
 
-    
-    
-    function Ingredients() {
-        return(
-        useEffect(() => {
-
-    
-    if(ingredient === true) {
-        
-        
-        
-        <table id="ingredients">
-                        
-        {
-            dataProduct.ingredients.map((i) => (
-                <>
-                <tr>
-
-                    <td>{i.text}</td>
-                    
-                    <td>{Math.round(i.percent_estimate)}%</td>
-                </tr>
-                
-                </>
-            ))
-        }
-
-        </table>;
-
-    } else {
-        
-        
-
-        
-        <ul id="nutrition">
-            <li>Valeurs nutritionelles pour 100g</li>
-            <li>Energie: {dataProduct.nutriments.energy_value} kcal</li>
-            <li>Protéines :{Math.round(dataProduct.nutriments.proteins)}g</li>
-            <li>Glucides :{Math.round(dataProduct.nutriments.carbohydrates)}g</li>
-            <li>Lipides : {Math.round(dataProduct.nutriments.fat)}g</li>
-            <li>Fibres : {Math.round(dataProduct.nutriments.fiber)}g</li>
-
-        </ul>
-    }
-            
-        
-}, []))}
 
 
     return (
@@ -83,7 +36,7 @@ function Product(props) {
           dataProduct != null &&
           <div className='productContainer'>
             <div className='productTitle'>
-                <h2>{dataProduct.generic_name}</h2>
+                <h2>{dataProduct.product_name}</h2>
                 <h4>{dataProduct.brands}</h4>
             </div>
             <img id="productImg" src={dataProduct.image_front_small_url} alt='Product'/>
@@ -94,18 +47,21 @@ function Product(props) {
                     {dataProduct.nutriscore_grade.toUpperCase()}
                 </p>
             </h3>
-            {/* <p>Allergènes: <b>{dataProduct.allergens_from_ingredients.split('en:')}</b></p> */}
 
             <div className='productInfos'>
 
                 <div className="productBtn">
-                    <button onClick={() => setIngredient(true)}> Ingrédients </button>
-                    <button onClick={() => setIngredient(false)}> Nutriments </button>
+                    <button style={ingredient === 'A' ? {background:'var(--yellow)', color:'var(--black)'} : {color:'var(--white)'}} onClick={() => setIngredient('A')}> Nutriments </button>
+                    <button style={ingredient === 'B' ? {background:'var(--yellow)', color:'var(--black)'} : {color:'var(--white)'}} onClick={() => setIngredient('B')}> Ingrédients </button>
+                    <button style={ingredient === 'C' ? {background:'var(--yellow)', color:'var(--black)'} : {color:'var(--white)'}} onClick={() => setIngredient('C')}> Informations </button>
                 </div>
 
-                <Ingredients/>
 
-                {/* <table id="ingredients">
+            {
+
+                ingredient === 'B' &&
+
+                <table id="ingredients">
                     
                         {
                             dataProduct.ingredients.map((i) => (
@@ -124,16 +80,37 @@ function Product(props) {
                 
 
                 </table>
+            }  
 
+           { 
+                ingredient === 'A' &&
+                <>
+                
+                <h3 id='ingredientsSubtitle'>Valeurs nutritionelles pour 100g :</h3>
                 <ul id="nutrition">
-                        <li>Valeurs nutritionelles pour 100g</li>
-                        <li>Energie: {dataProduct.nutriments.energy_value} kcal</li>
-                        <li>Protéines :{Math.round(dataProduct.nutriments.proteins)}g</li>
-                        <li>Glucides :{Math.round(dataProduct.nutriments.carbohydrates)}g</li>
+                        <li>Energie : {dataProduct.nutriments.energy_value} kcal</li>
+                        <li>Protéines : {Math.round(dataProduct.nutriments.proteins)}g</li>
+                        <li>Glucides : {Math.round(dataProduct.nutriments.carbohydrates)}g</li>
                         <li>Lipides : {Math.round(dataProduct.nutriments.fat)}g</li>
                         <li>Fibres : {Math.round(dataProduct.nutriments.fiber)}g</li>
 
-                </ul> */}
+                </ul>
+
+                </>
+
+           }    
+
+           {
+               ingredient === 'C' &&
+               <div className='productInformations'>
+
+                    <p>Quantité : {dataProduct.quantity}</p>
+                    <p>Conditionnement : {dataProduct.packaging}</p>
+                    <p>Conservation : {dataProduct.conservation_conditions}</p>
+                    <p>Allergènes : {dataProduct.allergens}</p>
+
+               </div>
+           }
 
             </div>    
             
